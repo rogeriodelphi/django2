8b8da3e02afc0470f6362b4dd95b4c26f8de0ec1
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-10-30 11:53:46 -0300

    Atualização

--- django2/settings.py
+++ django2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['django2-rbm.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django2-rbm.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
